8e593f0821c71efb09f36c12e75a00af8ff20645
Rikvalix <titouan.delion.desrochers@outlook.fr>
2026-05-19 22:50:04 +0200

    feat: utils to get current exec path

--- src/email/adapters/gmail.rs
+++ src/email/adapters/gmail.rs
@@ -2,14 +2,13 @@ extern crate google_gmail1 as gmail1;
 
 use crate::email::ports::email::EmailI;
 use crate::model::{Email, EmailStatus, Sender};
+use crate::utils::path_utils::get_current_exec_path;
 use chrono::DateTime;
 use gmail1::{hyper_rustls, hyper_util, yup_oauth2, Gmail};
 use google_gmail1::api::{Message, ModifyMessageRequest};
 use google_gmail1::hyper_rustls::HttpsConnector;
 use google_gmail1::hyper_util::client::legacy::connect::HttpConnector;
 use regex::Regex;
-use std::env;
-use std::path::PathBuf;
 use std::str::FromStr;
 
 pub struct GmailAdapter {
@@ -115,17 +114,13 @@ impl EmailI for GmailAdapter {
 
 impl GmailAdapter {
     pub async fn new() -> Self {
-        let mut path: PathBuf = if let Ok(cargo_dir) = env::var("CARGO_MANIFEST_DIR") {
-            let dev_path = PathBuf::from(cargo_dir);
-            dev_path
-        } else {
-            let mut exec_path = env::current_exe().unwrap();
-            exec_path.pop();
-            exec_path
-        };
-        path.push("client_secret.json");
+        let exec_path = get_current_exec_path();
+        
+        let mut json_path = exec_path.clone();
+        json_path.push("client_secret.json");
+
         let secret: yup_oauth2::ApplicationSecret =
-            yup_oauth2::read_application_secret(path)
+            yup_oauth2::read_application_secret(json_path)
                 .await
                 .expect("Fail to read application secret file: client_secret.json");
 
@@ -137,6 +132,9 @@ impl GmailAdapter {
             .build();
 
         let executor = hyper_util::rt::TokioExecutor::new();
+
+        let mut token_path = exec_path;
+        token_path.push("token.json");
         let auth = yup_oauth2::InstalledFlowAuthenticator::with_client(
             secret,
             yup_oauth2::InstalledFlowReturnMethod::HTTPRedirect,
@@ -144,7 +142,7 @@ impl GmailAdapter {
                 hyper_util::client::legacy::Client::builder(executor).build(connector),
             ),
         )
-        .persist_tokens_to_disk("token.json")
+        .persist_tokens_to_disk(token_path)
         .build()
         .await
         .unwrap();
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,2 @@
 pub mod file_format_utils;
+pub mod path_utils;
