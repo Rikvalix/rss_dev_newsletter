abea8cea8533a6b1e4d53cf0d781a241c01fe5a7
Rikvalix <titouan.delion.desrochers@outlook.fr>
2026-09-22 08:13:27 +0200

    fix: nullable SQL request

--- src/infrastructure/database/repository/ai_summary_repository.rs
+++ src/infrastructure/database/repository/ai_summary_repository.rs
@@ -34,19 +34,6 @@ impl AiSummaryRepository {
         Ok(saved)
     }
 
-    pub async fn find_latest(&self) -> Result<AiSummaryEntity, sqlx::Error> {
-        let response = sqlx::query_as::<_, AiSummaryEntity>(
-            r#"
-                SELECT id::bigint,ai_classification_id::bigint, content::json, created_at, updated_at FROM AI_SUMMARY
-                  ORDER BY ID::bigint DESC
-                  LIMIT 1
-                "#)
-            .fetch_one(&self.pool)
-            .await?;
-
-        Ok(response)
-    }
-
     pub async fn get_by_creation_date_with_feed_items(
         &self,
         date: &NaiveDate,
@@ -65,12 +52,12 @@ impl AiSummaryRepository {
 
         let feed_items: Vec<FeedItemEntity> = sqlx::query_as::<_, FeedItemEntity>(
             r#"
-            SELECT id::bigint, feed_id::bigint,guid,url,title,content,published_at, updated_at FROM feed_items  fi
+            SELECT id::bigint, feed_id::bigint,guid,url,title,content, updated_at FROM feed_items  fi
                 inner join ai_classification_feed_items acfi on acfi.feed_item_id = fi.id
                 where acfi.ai_classification_id = $1
             "#,
         )
-        .bind(ai_summary.id)
+        .bind(ai_summary.ai_classification_id)
         .fetch_all(&self.pool)
         .await?;
 
--- src/infrastructure/web/handler.rs
+++ src/infrastructure/web/handler.rs
@@ -1,9 +1,10 @@
 use crate::domain::application::model::ApplicationConfiguration;
+use crate::domain::database::model::SummaryEntity;
 use crate::domain::web::model::{HealthDto, MetadataDto, NewsletterDto, NewsletterShortDto};
 use crate::infrastructure::database::repository::summary_repository::SummaryRepository;
+use axum::Json;
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
-use axum::Json;
 
 #[derive(Debug,Clone)]
 pub struct WebHandler {
@@ -51,20 +52,25 @@ impl WebHandler {
         (StatusCode::OK,Json(Some(dto)))
     }
 
-    pub async fn get_latest_newsletter(State(state): State<Self>) -> (StatusCode, Json<NewsletterDto>) {
-        let latest_summary = state.summary_repository.find_latest().await.unwrap();
+    pub async fn get_latest_newsletter(State(state): State<Self>) -> (StatusCode, Json<Option<NewsletterDto>>) {
+        let latest_summary_opt: Option<SummaryEntity> = state.summary_repository.find_latest().await.unwrap();
+
+        if latest_summary_opt.is_none() {
+           return (StatusCode::NO_CONTENT, Json(None));
+        }
+        let entity = latest_summary_opt.unwrap();
 
         let dto = NewsletterDto {
-            public_id: latest_summary.public_id.to_string(),
-            date: latest_summary.created_at.date_naive(),
-            title: latest_summary.title.to_string(),
-            content: latest_summary.content.to_string(),
+            public_id: entity.public_id.to_string(),
+            date: entity.created_at.date_naive(),
+            title: entity.title.to_string(),
+            content: entity.content.to_string(),
             metadata: MetadataDto {
-                model: Some(latest_summary.metadata.model.clone().unwrap().to_string()),
+                model: Some(entity.metadata.model.clone().unwrap().to_string()),
             },
         };
 
-        (StatusCode::OK, Json(dto))
+        (StatusCode::OK, Json(Some(dto)))
     }
 
     pub async fn all_shorts(State(state) : State<Self>) -> (StatusCode, Json<Vec<NewsletterShortDto>>) {
